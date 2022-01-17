38bc54582a4cef7251c26c8742a99e417e924514
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2022-01-17 18:40:42 +0100

    Modified the status messages, and added lacking @Service decorator

--- src/rest-api/services/status/index.ts
+++ src/rest-api/services/status/index.ts
@@ -1,11 +1,13 @@
 import {RouteHandler} from "fastify";
+import {Service} from "typedi";
 
 interface IStatusService {
   index: RouteHandler
 }
 
+@Service()
 export class StatusService implements IStatusService {
-  index: RouteHandler = (req, reply) => {
-    reply.send(`Hello ${req.ip}`);
+  index: RouteHandler = (_req, reply) => {
+    reply.send({success: true});
   }
 }
