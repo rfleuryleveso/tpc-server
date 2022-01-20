2fdfb69d50aeaafaadae0c2747ff134713f3b218
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2022-01-20 18:13:54 +0100

    Reformatted & normalized mainService

--- src/mainService.ts
+++ src/mainService.ts
@@ -1,13 +1,13 @@
-import { Service } from 'typedi';
-import { AppLogger } from './services/logger';
-import { AppEnv } from './services/env';
-import { HttpServer } from './services/http-server';
-import { RestApi } from './rest-api/RestApi';
-import { NoReplyMailer } from './services/mailer';
+import {Service} from 'typedi';
+import {AppLogger} from './services/logger';
+import {AppEnv} from './services/env';
+import {HttpServer} from './services/http-server';
+import {RestApi} from './rest-api/RestApi';
+
 
 @Service()
 export class MainService {
-  constructor(private loggerService: AppLogger, private envService: AppEnv, private httpServer: HttpServer, private restApi: RestApi, private noReplyMailer: NoReplyMailer) {
+  constructor(private loggerService: AppLogger, private envService: AppEnv, private httpServer: HttpServer, private restApi: RestApi) {
   }
 
   /**
@@ -18,13 +18,8 @@ export class MainService {
     const env = this.envService.get('NODE_ENV');
     this.loggerService.info(`Starting TPC Server, in ${env}`);
     this.restApi.registerHttp();
-    this.noReplyMailer.createAccountMessage({
-      recipientCreateAccount: 'pblinfo59@gmail.com',
-      subjectCreateAccount: 'Creation de compte sur TPLC',
-      messageCreateAccount: 'merci de créer votre compte TPLC avec le lien suivant : ',
-      linkCreateAccount: 'zebi',
-    });
     await this.httpServer.start();
+    // await this.noReplyMailer.testConnection();
   }
 
 }
