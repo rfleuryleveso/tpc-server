bb70a3285cbfad4da9ba4b7b256a534c2dcf50c2
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2022-01-20 20:21:16 +0100

    Registered QRController to RestApi.ts

--- src/rest-api/RestApi.ts
+++ src/rest-api/RestApi.ts
@@ -2,6 +2,7 @@ import {Service} from "typedi";
 import {HttpServer} from "../services/http-server";
 import {AuthController} from "./controllers/auth";
 import {UsersController} from "./controllers/users";
+import {QrController} from "./controllers/qr";
 
 
 @Service()
@@ -10,12 +11,14 @@ export class RestApi {
     private httpServer: HttpServer,
     private authController: AuthController,
     private usersController: UsersController,
+    private qrController: QrController,
   ) {
   }
 
   registerHttp() {
     this.httpServer.register(this.authController.register, {prefix: '/auth'});
     this.httpServer.register(this.usersController.register, {prefix: '/users'});
+    this.httpServer.register(this.qrController.register, {prefix: '/qr'});
   }
 
 
