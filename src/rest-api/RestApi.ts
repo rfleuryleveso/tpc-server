7443e2da4e8930e2889598d2c0151be412bf3604
Raphaël Fleury-le veso <rfleuryleveso@gmail.com>
2022-01-20 17:02:16 +0100

    Added the users endpoint to the rest api

--- src/rest-api/RestApi.ts
+++ src/rest-api/RestApi.ts
@@ -1,15 +1,21 @@
 import {Service} from "typedi";
 import {HttpServer} from "../services/http-server";
-import {StatusController} from "./controllers/status";
+import {AuthController} from "./controllers/auth";
+import {UsersController} from "./controllers/users";
 
 
 @Service()
 export class RestApi {
-  constructor(private httpServer: HttpServer, private statusController: StatusController) {
+  constructor(
+    private httpServer: HttpServer,
+    private authController: AuthController,
+    private usersController: UsersController,
+  ) {
   }
 
   registerHttp() {
-    this.httpServer.register(this.statusController.register, {prefix: '/status'})
+    this.httpServer.register(this.authController.register, {prefix: '/auth'});
+    this.httpServer.register(this.usersController.register, {prefix: '/users'});
   }
 
 
